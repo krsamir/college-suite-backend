91e0ef1d2ca808b8b9bcb2071aac47482ed74dbf
samir <samir1741012321@gmail.com>
2021-04-15 10:35:10 +0530

    created student Login

--- app/models/authModels.js
+++ app/models/authModels.js
@@ -61,33 +61,6 @@ Auth.loginAdmin = (data, result) => {
   });
 };
 
-Auth.loginStudent = (data, result) => {
-  const { email, password } = data;
-  let query = `SELECT * FROM admin_master where email="${email}"`;
-  SQL.query(query, async (err, res) => {
-    if (err) {
-      console.log(err);
-      result(err, null);
-    } else {
-      if (res.length === 0) {
-        result(null, "usernotfound");
-      } else {
-        const response = JSON.parse(JSON.stringify(res))[0];
-        // const isMatched = await bcrypt.compare(password, response.password);
-        const isMatched = password === response.password;
-        if (isMatched) {
-          const token = jwt.sign({ id: email, role: "admin" }, JWT_SECRET, {
-            expiresIn: JWT_EXPIRATION_TIME,
-          });
-          return result(null, { token });
-        } else {
-          result(null, "invaliduser");
-        }
-      }
-    }
-  });
-};
-
 Auth.loginTeacher = (data, result) => {
   const { empId, password } = data;
   let query = `SELECT * FROM teacher_master where employee_id="${empId}"`;
@@ -114,4 +87,32 @@ Auth.loginTeacher = (data, result) => {
     }
   });
 };
+
+Auth.loginStudent = (data, result) => {
+  const { regdno, password } = data;
+  let query = `SELECT * FROM student_master where regd_no="${regdno}"`;
+  SQL.query(query, async (err, res) => {
+    if (err) {
+      console.log(err);
+      result(err, null);
+    } else {
+      if (res.length === 0) {
+        result(null, "usernotfound");
+      } else {
+        const response = JSON.parse(JSON.stringify(res))[0];
+        // const isMatched = await bcrypt.compare(password, response.password);
+        const isMatched = password === response.password;
+        if (isMatched) {
+          const token = jwt.sign({ id: regdno, role: "student" }, JWT_SECRET, {
+            expiresIn: JWT_EXPIRATION_TIME,
+          });
+          return result(null, { token });
+        } else {
+          result(null, "invaliduser");
+        }
+      }
+    }
+  });
+};
+
 export default Auth;
